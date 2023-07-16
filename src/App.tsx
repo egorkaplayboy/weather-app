2ccd85513ce402389b37202b6715c1eea39f5098
egorkaplayboy <bel04ka228@mail.ru>
2023-07-16 20:25:45 +0300

    Доделал верстку

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import MonthStatistics from "./pages/MonthStatistics/MonthStatistics";
 import Header from "./shared/Header/Header";
+import Popup from "./shared/Popup/Popup";
 
 function App() {
-  return <div className="container">
-    <Header/>
-    <Routes>
-      <Route path="/" Component={Home}/>
-      <Route path="/month-statistics" Component={MonthStatistics}/>
-    </Routes>
-  </div>;
+  return (
+    <div className="global-container">
+      <Popup/>
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/month-statistics" Component={MonthStatistics} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
